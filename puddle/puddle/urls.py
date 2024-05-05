65dc4a5a4bd46b7e8fd03aa8879eb19f97d7d75c
Aaron <a.genevision14@gmail.com>
2024-05-05 01:59:53 -0500

    Began developing signup page, elements, and functionality

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
 # Do not do the below code outside of development
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,9 +8,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index, name='index'),
+    path("", include('core.urls')),
     path('items/', include('item.urls')),
-    path("contact/", contact, name= 'contact'),
     # Do not do the below code outside of dev
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # Do not do the above code outside of dev
